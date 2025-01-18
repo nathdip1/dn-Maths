e8b1efc7dee1516d3f5341038ab53fa9f061e853
Dipjyoti <nathdipjyoti844@gmail.com>
2025-01-18 16:50:16 +0530

    added more methods

--- index.js
+++ index.js
@@ -6,15 +6,43 @@ const dnMaths = {
             return 1;
         return num*dnMaths.fact(num-1);
         },
-    randomdigit: function generateRandomDigit(num) {
-            const minDigitRequired = 10**(num-1);
-            const now = Date.now();
-            const microTime = performance.now();
-            let randomValue =((((now ^ microTime) >>> 0)*now)%minDigitRequired)
-            while(randomValue<minDigitRequired){
-                randomValue = randomValue + minDigitRequired;
+
+    randomdigit: function (num){
+        const minDigitRequired = 10**(num-1);
+        const now = Date.now();
+        const microTime = performance.now();
+        let randomValue =((((now ^ microTime) >>> 0)*now)%minDigitRequired)
+        while(randomValue<minDigitRequired){
+            randomValue = randomValue + minDigitRequired;
             }
-            return randomValue;
+        return randomValue;
+        },
+
+    seriesSum: function (num){
+        let sum = 0;
+        for(let i=1; i<=num; i++)
+            sum += i;
+        return sum;
+        },
+
+    seriesProduct: function (num){
+        let product = 1;
+        for(let i=1; i<=num; i++)
+            product *= i;
+        return product;
+        },
+
+    isPrime: function (num){
+        if(num===0||num===1 || num<0)
+            console.log("Error: Test a number GT 1");
+        else{
+            for(let i=2; i<=Math.sqrt(num); i++){
+                if(num%i===0)
+                    return false;
+                else
+                    return true;  
+                }
+            }
         }
     };
 
